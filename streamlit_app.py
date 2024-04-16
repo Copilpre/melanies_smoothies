dd035defd5b5ac3b78eacbaf0f92a23a3e9b4730
Copilpre <104974796+Copilpre@users.noreply.github.com>
2024-04-16 15:10:18 +0200

    Update streamlit_app.py
    json format for api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -49,6 +45,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
 
 
 
