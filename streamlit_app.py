3e9f1cc65e74ea1f1e57ff1b6b9231e21c55f30e
Copilpre <104974796+Copilpre@users.noreply.github.com>
2024-04-16 15:08:29 +0200

    Update streamlit_app.py
    added api requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
